e0084acfae63f6f2f839ade105d69fd8d8cd4f5d
11hussain <113888282+11hussain@users.noreply.github.com>
2022-11-20 21:47:30 +0530

    Update Main.java
    second merge to Master

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("FirstMergeExample");
         System.out.println("its some more Diffcult");
+        System.out.println("second merge to Master");
     }
 }
