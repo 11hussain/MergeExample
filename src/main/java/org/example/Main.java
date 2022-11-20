f1bbd10e5735ecfe4c592068a3c333766a06b806
11hussain <113888282+11hussain@users.noreply.github.com>
2022-11-20 22:46:44 +0530

    Update Main.java
    Merge the code to fifth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("FirstMergeExample");
-        System.out.println("its some more Diffcult");
+       
         System.out.println("second merge to Master");
     }
 }
